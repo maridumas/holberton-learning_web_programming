09266a780b32228db0803c311e6b5191062c59e9
Mari Dumas <johnson-dumasm19@bishopodowd.org>
2017-07-03 15:53:23 -0700

    grading system

---
+++ 6-javascript_continued/3-grading_system.js
@@ -0,0 +1,23 @@
+let parameter =  process.argv[2];
+let a = 5
+else if (parameter > 95) {
+  console.log("You have received an A+")
+}
+else if (parameter > 90) {
+  console.log("You have received an A")
+}
+else if (parameter > 85) {
+  console.log("You have received an B+")
+}
+else if (parameter > 80) {
+  console.log("You have received an B")
+}
+else if (parameter > 75) {
+  console.log("You have received an C+")
+}
+else if (parameter > 70) {
+  console.log("You have received an C")
+}
+else if (parameter > 65) {
+  console.log("You have received an D")
+}
