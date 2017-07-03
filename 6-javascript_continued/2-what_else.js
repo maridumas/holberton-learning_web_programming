7ca3757350a19396f8cf23cf0b47cb49fb51e603
Mari Dumas <johnson-dumasm19@bishopodowd.org>
2017-07-03 14:27:26 -0700

    quotations

--- 6-javascript_continued/2-what_else.js
+++ 6-javascript_continued/2-what_else.js
@@ -2,8 +2,8 @@ let parameter =  process.argv[2];
 let a = 5
 let b = my_parameter
 if (parameter > a) {
-  console.log(The value is greater than 5)
+  console.log("The value is greater than 5")
 }
 else {
-  console.log(The value is equal to 5 or lower)
+  console.log("The value is equal to 5 or lower")
 }
